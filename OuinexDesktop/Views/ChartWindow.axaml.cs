32b05be3c8dd63a92c19a2ba9ded689599cd28d4
BizUnity <128179084+BizUnity@users.noreply.github.com>
2023-03-27 18:34:36 +0200

    adding chart bases

--- OuinexDesktop/Views/ChartWindow.axaml.cs
+++ OuinexDesktop/Views/ChartWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+
 namespace OuinexDesktop.Views;
 
 public partial class ChartWindow : Window
@@ -12,4 +15,17 @@ public partial class ChartWindow : Window
     private void ChartWindow_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
     }
+}
+
+public class ViewModel : ViewModels.ViewModelBase
+{
+    public ISeries[] Series { get; set; }
+        = new ISeries[]
+        {
+                new LineSeries<double>
+                {
+                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                    Fill = null
+                }
+        };
 }

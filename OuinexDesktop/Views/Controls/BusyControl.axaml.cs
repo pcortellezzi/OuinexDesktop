bb8b874a67e7434fe43cb7ac3404e9afc1734218
BizUnity <128179084+BizUnity@users.noreply.github.com>
2023-04-09 19:52:28 +0200

    design

---
+++ OuinexDesktop/Views/Controls/BusyControl.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace OuinexDesktop.Views.Controls
+{
+    public partial class BusyControl : UserControl
+    {
+        public BusyControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

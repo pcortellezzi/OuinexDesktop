cc71b9db85e01d133acc31391834467a35beaebf
BizUnity <128179084+BizUnity@users.noreply.github.com>
2023-04-12 14:02:33 +0200

    adapting view model base

--- OuinexDesktop/ViewModels/ViewModelBase.cs
+++ OuinexDesktop/ViewModels/ViewModelBase.cs
@@ -4,5 +4,19 @@ namespace OuinexDesktop.ViewModels
 {
     public class ViewModelBase : ReactiveObject
     {
+        private bool _isEmptyOfData = false;
+        private bool _isBusy = false;
+
+        public bool IsEmptyOfData
+        {
+            get => _isEmptyOfData;
+            set => this.RaiseAndSetIfChanged(ref _isEmptyOfData, value, nameof(IsEmptyOfData)); 
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => this.RaiseAndSetIfChanged(ref _isEmptyOfData, value, nameof(IsBusy));
+        }
     }
 }

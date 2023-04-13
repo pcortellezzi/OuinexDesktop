3cc5d04836da41287ce9d5a6645dc9d09d8071c3
BizUnity <128179084+BizUnity@users.noreply.github.com>
2023-04-13 11:27:11 +0200

    don't know

---
+++ OuinexDesktop/Views/CustomControls/DataContainer.axaml.cs
@@ -0,0 +1,30 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace OuinexDesktop.Views.CustomControls
+{
+    public partial class DataContainer : UserControl
+    {
+        public DataContainer()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly StyledProperty<Geometry> PathProperty = AvaloniaProperty.Register<DataContainer, Geometry>(nameof(Path));
+
+        public Geometry Path
+        {
+            get { return GetValue(PathProperty); }
+            set { SetValue(PathProperty, value); }
+        }
+
+        public static readonly StyledProperty<bool> IsPathVisibleProperty = AvaloniaProperty.Register<DataContainer, bool>(nameof(IsPathVisible));
+
+        public bool IsPathVisible
+        {
+            get { return GetValue(IsPathVisibleProperty); }
+            set { SetValue(IsPathVisibleProperty, value); }
+        }
+    }
+}

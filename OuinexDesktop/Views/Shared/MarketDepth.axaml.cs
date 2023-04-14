2fbf9cee4d2150d7411ed82ef0012f9a3c4739fe
BizUnity <128179084+BizUnity@users.noreply.github.com>
2023-04-14 10:31:01 +0200

    adding marketdepth control

---
+++ OuinexDesktop/Views/Shared/MarketDepth.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace OuinexDesktop.Views.Shared
+{
+    public partial class MarketDepth : UserControl
+    {
+        public MarketDepth()
+        {
+            InitializeComponent();
+        }
+    }
+}
